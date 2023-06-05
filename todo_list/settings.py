e43c45d677f593053608b2c7a036d5c8751e7848
Vaidik Gupta <vaidikgupta2000@gmail.com>
2023-06-05 19:43:10 +0530

    allowed host changes

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-+buz5)+sd=d4&sthgb-vc&o20_5top=grns3q-hg4evr%25wzj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
